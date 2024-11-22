c2a66b3d6297f2b88656f726e60665cd342d2c5f
Levy-Jr <levyjrgomes@gmail.com>
2024-11-21 20:19:27 -0400

    feat: anuncie and encomende-um-imovel page layout

---
+++ app/encomende-um-imovel/components/encomende-form.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+const Form = () => {
+  return (
+    <form className="relative w-[min(100%,31.25rem)] bg-white mx-auto rounded-[.625rem] my-[4.6875rem] p-5 before:absolute before:inset-0 before:-z-10 before:-m-[.125rem] before:rounded-[inherit] before:bg-gradient-to-r before:from-lightBrown before:to-darkBrown">
+      <h2 className="font-newsReader text-3xl text-grayAccent text-center">ANUNCIE NO SITE</h2>
+      <div className="text-grayAccent *:placeholder:text-black my-4 grid gap-[.625rem] *:pl-5 *:rounded-[100vmax] *:outline-none *:py-2 *:border-[.0625rem] *:border-[#F2F2F2]">
+        <input
+          type="text"
+          placeholder="Nome"
+        />
+        <input
+          type="text"
+          placeholder="E-mail"
+        />
+        <input
+          type="text"
+          placeholder="Telefone"
+        />
+        <textarea
+          className="resize-none"
+          placeholder="Deixe sua mensagem"
+        />
+      </div>
+      <div className="text-center">
+        <p>Prefiro contato via:</p>
+        <div className="flex items-center gap-5 justify-center">
+          <label>
+            <input type="radio" name="contact" /> <span>WhatsApp</span>
+          </label>
+          <label>
+            <input type="radio" defaultChecked name="contact" /> <span>E-mail</span>
+          </label>
+          <label>
+            <input type="radio" name="contact" /> <span>Telefone</span>
+          </label>
+        </div>
+      </div>
+      <div className="mt-[1.875rem]">
+        <button className="bg-[#95a3ab] hover:bg-lightBrown py-2 text-lg block w-[min(100%,15.625rem)] text-center mx-auto text-white rounded-[100vmax]">ENVIAR</button>
+      </div>
+    </form>
+  )
+}
+
+export default Form
---
+++ app/encomende-um-imovel/page.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image"
+import TelIcon from "@/public/talkToUs/tel-icon.svg"
+import WppBtnIcon from "@/public/talkToUs/wpp-btn-icon.svg"
+import Form from "./components/encomende-form"
+
+const Anuncie = () => {
+  return (
+    <main className="w-lg-container mt-20 mx-auto">
+      <div
+        className="fixed bg-[top_center] bg-fixed -z-10 inset-0"
+        style={{
+          backgroundImage: `url('/marble-bg.webp')`
+        }}
+      />
+      <h1 className="font-newsReader text-4xl text-center mb-10 text-grayAccent">FALE COM A W INVESTMENTS</h1>
+      <div className="text-white *:py-2 *:rounded-[100vmax] flex flex-col sm:flex-row gap-4 *:w-[15.625rem] w-[min(100%,50rem)] mx-auto text-lg items-center sm:justify-between *:flex *:justify-center *:items-center *:gap-2 *:font-bold">
+        <button className="bg-[#95a3ab] hover:bg-darkBrown">
+          <Image
+            className="w-[1.375rem]"
+            src={WppBtnIcon}
+            alt="Whatsapp"
+          />
+          WhatsApp
+        </button>
+        <a className="bg-[#95a3ab] hover:bg-darkBrown" href="tel:5541997683471">
+          <Image
+            className="w-[1.375rem]"
+            src={TelIcon}
+            alt="Telefone"
+          />
+          Ligar
+        </a>
+      </div>
+      <Form />
+    </main>
+  )
+}
+
+export default Anuncie
